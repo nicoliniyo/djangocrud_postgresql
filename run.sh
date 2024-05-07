d18756015357b5772481b9db3c061c11c46213df
Nicolai Abruzzese <nicoliniyo@gmail.com>
2024-05-06 20:41:09 -0400

    fix(devops): deploy err 500

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #python manage.py runserver
 
-python -m gunicorn tasks.asgi:application -k uvicorn.workers.UvicornWorker
+python -m gunicorn djangocrud.asgi:application -k uvicorn.workers.UvicornWorker
