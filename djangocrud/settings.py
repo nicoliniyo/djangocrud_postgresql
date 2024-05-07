50e0a293484b8fb0442f177471d15311082fff2b
Nicolai Abruzzese <nicoliniyo@gmail.com>
2024-05-06 21:27:28 -0400

    fix(devops): static files missing on deployment, storage change package name

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -143,7 +143,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
