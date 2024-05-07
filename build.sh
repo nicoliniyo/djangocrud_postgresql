995c59de1e6451f0c479372efbd7e45593938e30
Nicolai Abruzzese <nicoliniyo@gmail.com>
2024-05-06 21:00:18 -0400

    fix(devops): deployment statif files missing

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
+echo('COLLECT STATIC')
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
+echo('DB MIGRATE')
 python manage.py migrate
